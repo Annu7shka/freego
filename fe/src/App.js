20167203bcd90ab63818e781d6e3dd3847f79ea5
annu7shka <annu7shka@gmail.com>
2018-02-08 15:02:47 -0500

    Add bootstrap, BE integration

--- fe/src/App.js
+++ fe/src/App.js
@@ -3,6 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    fetch('http://localhost:3001/events/index.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        this.setState({
+          events: data.events
+        });
+
+        console.log(data.events);
+      });
+  }
+
+  eventsTable(events) {
+    return events.map((ev, i) => {
+      return(
+        <tr key={i}>
+          <td>{ev.event_type}</td>
+          <td>{ev.title}</td>
+          <td>{ev.description}</td>
+        </tr>
+      );
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,11 +34,29 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <table>
+          <tbody>
+            <tr>
+              <th>Type</th>
+              <th>Title</th>
+              <th>Description</th>
+            </tr>
+            {this.eventsTable(this.state.events)}
+          </tbody>
+        </table>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" className="btn btn-primary">Primary</button>
       </div>
     );
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
   }
 }
 
