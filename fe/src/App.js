8e309c9ac2fa52b19ae059900d9b62951ee7578a
Tina Glock <tina.g@speedandfunction.com>
2018-02-11 13:44:08 -0500

    Remove unnecessary content

--- fe/src/App.js
+++ fe/src/App.js
@@ -81,7 +81,7 @@ class App extends Component {
                   <img className="card-img-top" src="..." alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
                 </div>
@@ -91,7 +91,7 @@ class App extends Component {
                   <img className="card-img-top" src="..." alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
                 </div>
@@ -103,7 +103,7 @@ class App extends Component {
                   <img className="card-img-top" src="..." alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
                 </div>
@@ -113,7 +113,7 @@ class App extends Component {
                   <img className="card-img-top" src="..." alt="Card image cap" />
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the content.</p>
                     <a href="#" className="btn btn-primary">Go somewhere</a>
                   </div>
                 </div>
