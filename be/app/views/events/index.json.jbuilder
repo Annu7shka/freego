0e5ba8411c732511199580ee02f73da7c323f205
Tina Glock <tina.g@speedandfunction.com>
2018-02-14 13:51:35 -0500

    Add paperclip and images processing

--- be/app/views/events/index.json.jbuilder
+++ be/app/views/events/index.json.jbuilder
@@ -1,1 +1,11 @@
-json.events @events
+json.array! @events do |event|
+  json.created_at event.created_at
+  json.event_type event.event_type
+  json.title event.title
+  json.description event.description
+  json.latitude event.latitude
+  json.longitude event.longitude
+  json.age_start event.age_start
+  json.age_end event.age_end
+  json.image_url event.image.url
+end
--- be/db/schema.rb
+++ be/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180213182506) do
+ActiveRecord::Schema.define(version: 20180214161800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,10 @@ ActiveRecord::Schema.define(version: 20180213182506) do
     t.datetime "updated_at", null: false
     t.integer "age_start"
     t.integer "age_end"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
