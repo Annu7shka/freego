d20dfcfb985dab9210314bb63e4952607f78ff8b
Russ Zhurba <russ@speedandfunction.com>
2018-03-07 18:31:42 -0500

    Add root route

--- be/config/routes.rb
+++ be/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  root to: 'pages#main'
   get 'events/index'
   get 'slides/index'
   get '*path', to: 'static#homepage'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
