174cf5dc5b0c4dfb61026e3372d3fe98dbd4173d
Russ Zhurba <russ@speedandfunction.com>
2018-03-07 18:40:49 -0500

    Fix root path

--- be/config/routes.rb
+++ be/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  root to: 'pages#main'
+  root to: 'static#homepage'
   get 'events/index'
   get 'slides/index'
   get '*path', to: 'static#homepage'
